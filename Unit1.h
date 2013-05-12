38215ed1daae445a593dd0b49c8a208a4f1de493
mskubenich <misha.skubenich@gmail.com>
2013-05-12 11:50:33 +0300

    visual builder

--- Unit1.h
+++ Unit1.h
@@ -7,13 +7,26 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
 	TMemo *Memo1;
 	TButton *Learn;
+	TGroupBox *GroupBox1;
+	TLabel *Label1;
+	TEdit *Edit1;
+	TScrollBox *ScrollBox1;
+	TEdit *Edit2;
+	TButton *Button2;
+	TGroupBox *GroupBox2;
+	TScrollBox *ScrollBox2;
+	TImage *Image1;
 	void __fastcall LearnClick(TObject *Sender);
+	void __fastcall Edit1Change(TObject *Sender);
+	void __fastcall Edit1KeyPress(TObject *Sender, System::WideChar &Key);
+	void __fastcall Button2Click(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
